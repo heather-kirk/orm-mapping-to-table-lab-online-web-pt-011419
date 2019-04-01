c55482c61e28f8916d4cf4b2348675dd5d61f216
Heather Kirk <hskirk@gmail.com>
2019-04-01 22:33:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
   SQL
   
   DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_row_id")
+  @id = DB[:conn].execute("SELECT last_insert_row_id() FROM students")
   
   end 
   
